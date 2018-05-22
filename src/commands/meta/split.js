d6798f7d0af21a37d89fa292a0ac8c08ce109be9
Kiril Kirov <k2kirov@gmail.com>
2018-05-22 17:14:42 +0300

    Added proper log message when splitting

--- src/commands/meta/split.js
+++ src/commands/meta/split.js
@@ -1,6 +1,7 @@
 /* eslint no-unused-vars:0, no-unused-expressions:0 */
 const yargs = require('yargs'),
 Davos = require('davos');
+Log = Davos.Logger;
 exports.command = 'split'
 exports.aliases = ['s']
 exports.desc = 'Split metadata'
@@ -11,5 +12,6 @@ exports.builder = {
   }
 };
 exports.handler = (argv) => {
+   Log.error("Metadata split in progress");
    new Davos.Core(argv, false).split();
 }
